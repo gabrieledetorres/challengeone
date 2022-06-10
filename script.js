4b86c76aad54ff819996c838fcd8b7edea88ad03
gabrieletorres <secgabriele@gmail.com>
2022-06-09 21:26:27 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let string="Gabi";
+let numero="2";
+let anoAtual="2022";
+let operacao = anoAtual - numero;
+console.log("Estou mexendo no console.log");
+let  quartaFeira = true;
+console.log(typeof quartaFeira);
